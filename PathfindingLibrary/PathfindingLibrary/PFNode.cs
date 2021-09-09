b0867a5670b40410989cdb45bea386d73f7e5ef4
Will <will.tripp@btinternet.com>
2021-09-09 19:27:23 +0100

    Added BakeMesh helper function and editied PFNode to accomodate this

---
+++ PathfindingLibrary/PathfindingLibrary/PFNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfindingLibrary
+{
+    public class PFNode
+    {
+        private int g;
+        private int h;
+        private List<PFNode> incidentNodes;
+
+        public PFNode(int cost)
+        {
+
+        }
+
+        public void AddIncidentNode(PFNode node) { incidentNodes.Add(node); }
+    }
+}
--- PathfindingLibrary/PathfindingLibrary/Pathfinder.cs
+++ PathfindingLibrary/PathfindingLibrary/Pathfinder.cs
@@ -5,9 +5,51 @@ namespace PathfindingLibrary
 {
     public class Pathfinder<T>
     {
-        public Queue<T> getPath(INavMesh<T> mesh,T from,T to)
+        INavMesh<T> rawMesh;
+
+        public Pathfinder(INavMesh<T> mesh)
+        {
+            rawMesh = mesh;
+        }
+
+        public Queue<T> getPath(T from,T to)
         {
             return null;
+        }
+
+        private List<PFNode> bakeMesh()
+        {
+            T[] objects = rawMesh.allObjects;
+            Dictionary<T, PFNode> convertedTo = new Dictionary<T, PFNode>();
+            List<PFNode> pFNodes = new List<PFNode>();
+            for(int i = 0;i < objects.Length; i++)
+            {
+                PFNode newNode = rawMesh.NodeFromObject(objects[i]);
+                convertedTo.Add(objects[i], newNode);
+                pFNodes.Add(newNode);
+            }
+
+            for(int i = 0;i < objects.Length; i++)
+            {
+                PFNode currentNode;
+                T currentObject = objects[i];
+                convertedTo.TryGetValue(currentObject, out currentNode);
+                T[] incidentObjects = rawMesh.GetIncidentObjects(currentObject);
+                for(int j = 0;j < incidentObjects.Length; j++)
+                {
+                    PFNode currentIncidentNode;
+                    if(convertedTo.TryGetValue(incidentObjects[j],out currentIncidentNode))
+                    {
+                        currentNode.AddIncidentNode(currentIncidentNode);
+                    }
+                    else
+                    {
+                        throw new Exception("Object from GetIncidentObjects not contain in allObjects");
+                    }
+                }
+            }
+
+            return pFNodes;
         }
     }
 }
