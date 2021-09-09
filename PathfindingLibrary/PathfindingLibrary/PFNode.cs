542eae1c9ee51b9dd89c09adc7ab0c363f8f6b73
Will <will.tripp@btinternet.com>
2021-09-09 19:49:32 +0100

    Added coordinate system

--- PathfindingLibrary/PathfindingLibrary/PFNode.cs
+++ PathfindingLibrary/PathfindingLibrary/PFNode.cs
@@ -4,15 +4,37 @@ using System.Text;
 
 namespace PathfindingLibrary
 {
+    public struct Coordinates
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public Coordinates(float x,float y,float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public Coordinates(float x,float y)
+        {
+            this.x = x;
+            this.y = y;
+            z = 0;
+        }
+    }
+
     public class PFNode
     {
-        private int g;
+        private readonly Coordinates coords;
+        private readonly int g;
         private int h;
         private List<PFNode> incidentNodes;
 
-        public PFNode(int cost)
+        public PFNode(int cost,Coordinates coords)
         {
-
+            this.coords = coords;
+            g = cost;
         }
 
         public void AddIncidentNode(PFNode node) { incidentNodes.Add(node); }
