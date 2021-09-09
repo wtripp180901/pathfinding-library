24c9f3bf23b5e127db0049d55aabdbd9a7844c9c
Will <will.tripp@btinternet.com>
2021-09-09 18:22:12 +0100

    Initial commit

---
+++ PathfindingLibrary/PathfindingLibrary/INavMesh.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PathfindingLibrary
+{
+    public interface INavMesh<T>
+    {
+
+    }
+}
---
+++ PathfindingLibrary/PathfindingLibrary/Pathfinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PathfindingLibrary
+{
+    public class Pathfinder<T>
+    {
+        public Queue<T> getPath(INavMesh<T> mesh,T from,T to)
+        {
+            return null;
+        }
+    }
+}
