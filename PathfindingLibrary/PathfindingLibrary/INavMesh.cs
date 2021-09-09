0a0986f015ed94f13a449c2b2eeab286e00e7b2f
Will <will.tripp@btinternet.com>
2021-09-09 18:38:47 +0100

    Added interface requirements

--- PathfindingLibrary/PathfindingLibrary/INavMesh.cs
+++ PathfindingLibrary/PathfindingLibrary/INavMesh.cs
@@ -6,6 +6,8 @@ namespace PathfindingLibrary
 {
     public interface INavMesh<T>
     {
-
+        T[] GetIncidentObjects(T incidentTo);
+        T AllObjects { get; }
+        PFNode NodeFromObject(T source);
     }
 }
